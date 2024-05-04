4477cacda39dc92a34c8d62032cf71adc1f9f50b
Allan Ayson <106575550+ayson18@users.noreply.github.com>
2024-05-04 00:45:32 +0000

    activity

---
+++ Main.java
@@ -0,0 +1,58 @@
+public class Main{
+
+    public static void main(String[] args) {
+        Car myCar = new Car("Toyota", "Camry", 2022, 4);
+
+        myCar.displayDetails();
+    }
+}
+
+
+         class Vehicle {
+            private final String make;
+            private final String model;
+            private final int year;
+
+            public Vehicle(String make, String model, int year) {
+                this.make = make;
+                this.model = model;
+                this.year = year;
+            }
+            public String getMake() {
+                return make;
+            }
+            public String getModel() {
+                return model;
+            }
+
+            public int getYear() {
+                return year;
+            }
+
+        }
+
+        class Car extends Vehicle {
+            private final int numberOfDoors;
+
+            public Car(String make, String model, int year, int numberOfDoors) {
+                super(make, model, year);
+                this.numberOfDoors = numberOfDoors;
+            }
+
+            
+            public void displayDetails() {
+                System.out.println("Car Details:");
+                System.out.println("Make: " + getMake());
+                System.out.println("Model: " + getModel());
+                System.out.println("Year: " + getYear());
+                System.out.println("Number of Doors: " + numberOfDoors);
+            }
+
+
+        }
+        
+
+
+
+
+
